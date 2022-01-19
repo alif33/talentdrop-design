b37250114b44fa9349951891cf46d2a97ecaa6c1
Hasan0734 <81487381+Hasan0734@users.noreply.github.com>
2022-01-20 03:23:54 +0600

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,5 @@
-import Layout from '../src/components/layout';
-import Login from '../src/components/login/Login';
+import Layout from '../src/components/client/layout';
+import Login from '../src/components/client/login/Login';
 
 export default function Loginpage() {
   return (
