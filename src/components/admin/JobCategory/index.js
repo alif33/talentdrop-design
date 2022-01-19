0e4d30a712945769ed0e119a908086eb55200fe1
Jahid Hasan <jahidhasan2856@gmail.com>
2022-01-19 22:26:56 +0600

    admin panel update

--- src/components/admin/JobCategory/index.js
+++ src/components/admin/JobCategory/index.js
@@ -3,7 +3,7 @@ import Layout from "../Layout";
 const JobCategorpy = () => {
     return (
         <Layout>
-            <h1>Hello Rakib, How Are you?</h1>
+            <h1>Job Category</h1>
         </Layout>
     );
 };
